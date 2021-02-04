6a732abddb4a7fe7849082b9fd4f2331b8de730a
serge klokov <sergeklokov@users.noreply.github.com>
2021-02-03 21:29:11 -0700

    Updated non-regex version

--- ReadMe.txt
+++ ReadMe.txt
@@ -9,3 +9,17 @@
 Let's use ECMA script 
 1st version with Regex
 2nd version without
+
+
+Output:
+
+PS C:\..\validateusername> node validateUserName
+"Se_rg" true
+"Serg_" false
+"Serge_Klokov" true
+"Serge2" true
+"undefined" false
+"null" false
+"Ser" false
+"Serge__Klokov" false
+"2Serge" false
--- validateUserName.js
+++ validateUserName.js
@@ -1,9 +1,9 @@
 // ================================================================
 // I am trying to validate a username. I want to have a regex that,
-// At least 4 characters long.
-// Must contain only letters, numbers and an optional underscore
-// It must not end with an underscore
-// It must start with a letter
+// 1. At least 4 characters long.
+// 2. Must contain only letters, numbers and an optional underscore
+// 3. It must not end with an underscore
+// 4. It must start with a letter
 // ================================================================
 
 function validate(username){
@@ -30,6 +30,20 @@ function isAlphabetic(char){
         });
 }
 
+function isNumeric(char){
+    var c = String(char);
+
+    var numbers = "0123456789".split("");
+
+    return numbers.some((letter) => {
+            return c === letter;
+        });
+}
+
+function isAlphanumeric(char){
+    return isAlphabetic(char) || isNumeric(char);
+}
+
 // let's don't use Regex
 function validate_v2(username){
     if (username === undefined || username === null)
@@ -39,27 +53,50 @@ function validate_v2(username){
     if (s.length < 4)
         return false;
 
-    var first = s.charAt(0);
-    var isFirstAlphabeting = isAlphabetic(first);
-    if (!isFirstAlphabeting)
-        return false;
-    
+    var u = username.split("");
+    var isUnderscorMet = false;
+    for(i=0; i<u.length; i++){
+        // check first character is alphabetical only
+        if (i == 0){
+            if (!isAlphabetic(u[i]))
+                return false;
+        }
+
+        // middle is alphanumeric or underscore
+        if (i > 0 && i < u.length - 1){
+            if (!isAlphanumeric(u[i]) && u[i] != "_") 
+                return false;
+
+            // check for only one underscore
+            if (u[i] == "_"){
+                if (isUnderscorMet)
+                    return false;
+                else
+                    isUnderscorMet = true;
+            }
+        }
+
+        // last is only alphanumeric 
+        if (i == u.length - 1 ){
+            if (!isAlphanumeric(u[i]))
+                return false;
+        }
+    }
+        
     return true;
 }
 
-console.log('Serge ' + validate_v2('Serge')); 
 
-// console.log('undefind ' + validate_v2()); // invalid user name
-// console.log('null ' + validate_v2(null)); // invalid user name
-// console.log('Ser ' + validate_v2('Ser')); // invalid user name
-// console.log('Serge_Klokov ' + validate_v2('Serge_Klokov')); // valid user name
-// console.log('Serge__Klokov ' + validate_v2('Serge__Klokov')); 
-// console.log('Serge Klokov ' + validate_v2('Serge Klokov')); // invalid user name
-// console.log('Serge_Klokov1 ' + validate_v2('Serge_Klokov1')); 
-// console.log('Serge_Klokov22 ' + validate_v2('Serge_Klokov22')); 
-// console.log('Serge ' + validate_v2('Serge')); 
-// console.log('3Serge ' + validate_v2('3Serge')); 
-// console.log('Serge_ ' + validate_v2('Serge_')); 
-// console.log('Ser.ge ' + validate_v2('Ser.ge')); 
-// console.log('Ser-ge ' + validate_v2('Ser-ge')); 
-// console.log('Serge+ ' + validate_v2('Serge+')); 
+function test(name){
+    console.log('"' + name + '" ' + validate_v2(name)); 
+}
+
+test('Se_rg');
+test('Serg_');
+test('Serge_Klokov');
+test('Serge2');
+test();
+test(null);
+test('Ser');
+test('Serge__Klokov');
+test('2Serge');
